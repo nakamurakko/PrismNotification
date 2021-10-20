e67680e30815266f7dda95c2bb5465220700e58b
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2021-10-20 15:02:13 +0900

    サンプルを移動。

---
+++ Sample/SampleWPFApp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Ioc;
+using PrismNotification.Dialogs;
+using SampleWPFApp.Views;
+using System.Windows;
+
+namespace SampleWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // https://prismlibrary.com/docs/wpf/dialog-service.html
+            NotificationDialogService.RegisterDialog(containerRegistry);
+        }
+    }
+}
---
+++ Sample/SampleWPFApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using PrismNotification.Dialogs;
+
+namespace SampleWPFApp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private IDialogService _dialogService;
+
+        private string _title = "Sample WPF App";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _clickResult = "";
+        public string ClickResult
+        {
+            get => _clickResult;
+            set => SetProperty(ref _clickResult, value);
+        }
+
+        private DelegateCommand _showMessageCommand;
+        public DelegateCommand ShowMessageCommand
+        {
+            get => _showMessageCommand;
+            set => SetProperty(ref _showMessageCommand, value);
+        }
+
+        /// <summary>
+        /// コンストラクター。
+        /// </summary>
+        /// <param name="dialogService">Dialog Service</param>
+        /// <remarks>
+        /// https://prismlibrary.com/docs/wpf/dialog-service.html#using-the-dialog-service
+        /// </remarks>
+        public MainWindowViewModel(IDialogService dialogService)
+        {
+            this._dialogService = dialogService;
+
+            ShowMessageCommand = new DelegateCommand(() => ShowMessageCommandExecute());
+        }
+
+        private void ShowMessageCommandExecute()
+        {
+            NotificationDialogService.ShowDialog(
+                _dialogService,
+                "Hello world.",
+                NotificationDialogButtons.YesNoCancel,
+                dialogResult =>
+                {
+                    switch (dialogResult.Result)
+                    {
+                        case ButtonResult.Yes:
+                            ClickResult = "Yes button clicked.";
+                            break;
+                        case ButtonResult.No:
+                            ClickResult = "No button clicked.";
+                            break;
+                        case ButtonResult.Cancel:
+                            ClickResult = "Cancel button clicked.";
+                            break;
+                        default:
+                            break;
+                    }
+                });
+        }
+    }
+}
