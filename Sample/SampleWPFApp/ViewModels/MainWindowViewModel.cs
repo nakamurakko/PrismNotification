59945fb3de3095390fa84b0a37162765cb257093
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-08-14 22:13:08 +0900

    コード整理。

--- Sample/SampleWPFApp/ViewModels/MainWindowViewModel.cs
+++ Sample/SampleWPFApp/ViewModels/MainWindowViewModel.cs
@@ -3,90 +3,89 @@ using Prism.Mvvm;
 using Prism.Services.Dialogs;
 using PrismNotification.Dialogs;
 
-namespace SampleWPFApp.ViewModels
+namespace SampleWPFApp.ViewModels;
+
+public sealed class MainWindowViewModel : BindableBase
 {
-    public class MainWindowViewModel : BindableBase
+    private IDialogService _dialogService;
+
+    private string _title = "Sample WPF App";
+    public string Title
     {
-        private IDialogService _dialogService;
+        get => this._title;
+        set => this.SetProperty(ref this._title, value);
+    }
 
-        private string _title = "Sample WPF App";
-        public string Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value);
-        }
+    private string _clickResult = "";
+    public string ClickResult
+    {
+        get => this._clickResult;
+        set => this.SetProperty(ref this._clickResult, value);
+    }
 
-        private string _clickResult = "";
-        public string ClickResult
-        {
-            get => _clickResult;
-            set => SetProperty(ref _clickResult, value);
-        }
+    private DelegateCommand _showMessageCommand;
+    public DelegateCommand ShowMessageCommand
+    {
+        get => this._showMessageCommand;
+        set => this.SetProperty(ref this._showMessageCommand, value);
+    }
 
-        private DelegateCommand _showMessageCommand;
-        public DelegateCommand ShowMessageCommand
-        {
-            get => _showMessageCommand;
-            set => SetProperty(ref _showMessageCommand, value);
-        }
+    private DelegateCommand _showOkMessageCommand;
 
-        private DelegateCommand _showOkMessageCommand;
+    public DelegateCommand ShowOkMessageCommand
+    {
+        get => this._showOkMessageCommand;
+        set => this.SetProperty(ref this._showOkMessageCommand, value);
+    }
 
-        public DelegateCommand ShowOkMessageCommand
-        {
-            get => _showOkMessageCommand;
-            set => SetProperty(ref _showOkMessageCommand, value);
-        }
+    /// <summary>
+    /// コンストラクター。
+    /// </summary>
+    /// <param name="dialogService">Dialog Service</param>
+    /// <remarks>
+    /// https://prismlibrary.com/docs/wpf/dialog-service.html#using-the-dialog-service
+    /// </remarks>
+    public MainWindowViewModel(IDialogService dialogService)
+    {
+        this._dialogService = dialogService;
 
-        /// <summary>
-        /// コンストラクター。
-        /// </summary>
-        /// <param name="dialogService">Dialog Service</param>
-        /// <remarks>
-        /// https://prismlibrary.com/docs/wpf/dialog-service.html#using-the-dialog-service
-        /// </remarks>
-        public MainWindowViewModel(IDialogService dialogService)
-        {
-            this._dialogService = dialogService;
+        this.ShowMessageCommand = new DelegateCommand(() => this.ShowMessageCommandExecute());
+        this.ShowOkMessageCommand = new DelegateCommand(() => this.ShowOkMessageCommandExecute());
+    }
 
-            ShowMessageCommand = new DelegateCommand(() => ShowMessageCommandExecute());
-            ShowOkMessageCommand = new DelegateCommand(() => ShowOkMessageCommandExecute());
-        }
-
-        /// <summary>
-        /// Yes、No、Cancel を表示するメッセージダイアログを表示する。
-        /// </summary>
-        private void ShowMessageCommandExecute()
-        {
-            NotificationDialogService.ShowDialog(
-                _dialogService,
-                "Hello world.",
-                NotificationDialogButtons.YesNoCancel,
-                dialogResult =>
+    /// <summary>
+    /// Yes、No、Cancel を表示するメッセージダイアログを表示する。
+    /// </summary>
+    private void ShowMessageCommandExecute()
+    {
+        NotificationDialogService.ShowDialog(
+            this._dialogService,
+            "Hello world.",
+            NotificationDialogButtons.YesNoCancel,
+            dialogResult =>
+            {
+                switch (dialogResult.Result)
                 {
-                    switch (dialogResult.Result)
-                    {
-                        case ButtonResult.Yes:
-                            ClickResult = "Yes button clicked.";
-                            break;
-                        case ButtonResult.No:
-                            ClickResult = "No button clicked.";
-                            break;
-                        case ButtonResult.Cancel:
-                            ClickResult = "Cancel button clicked.";
-                            break;
-                        default:
-                            break;
-                    }
-                });
-        }
+                    case ButtonResult.Yes:
+                        ClickResult = "Yes button clicked.";
+                        break;
+                    case ButtonResult.No:
+                        ClickResult = "No button clicked.";
+                        break;
+                    case ButtonResult.Cancel:
+                        ClickResult = "Cancel button clicked.";
+                        break;
+                    default:
+                        break;
+                }
+            });
+    }
 
-        /// <summary>
-        /// OK ボタンのみのメッセージダイアログを表示する。
-        /// </summary>
-        private void ShowOkMessageCommandExecute()
-        {
-            NotificationDialogService.ShowDialog(_dialogService, "OK Message.", NotificationDialogButtons.Ok);
-        }
+    /// <summary>
+    /// OK ボタンのみのメッセージダイアログを表示する。
+    /// </summary>
+    private void ShowOkMessageCommandExecute()
+    {
+        NotificationDialogService.ShowDialog(this._dialogService, "OK Message.", NotificationDialogButtons.Ok);
     }
 }
